7b3b516555c8110a6ea2d731094fdc5dd03a9f4c
waferthinninja <waferthinninja@gmail.com>
2016-01-05 23:43:33 +0000

    Added parallax scrolling background

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 newPos = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y,
+        -10);
+
+        this.transform.position = newPos;
+	
+	}
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+    public float parallax;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    MeshRenderer mr = GetComponent<MeshRenderer>();
+	    Material mat = mr.material;
+	    Vector2 offset = mat.mainTextureOffset;
+
+	    offset.x = transform.position.x / transform.localScale.x / parallax;
+        offset.y = transform.position.y / transform.localScale.y / parallax;
+
+	    mat.mainTextureOffset = offset;
+
+	}
+}

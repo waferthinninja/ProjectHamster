ffbac183ab3b8100b6e8c8e0ebfd903c890638a1
waferthinninja <waferthinninja@gmail.com>
2016-01-05 22:06:50 +0000

    Initial commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using EZ_Pooling;
+
+public class BulletController : MonoBehaviour {
+
+    public float TimeToLive;
+
+    private float age;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        age += Time.deltaTime;
+
+        if (age > TimeToLive)
+            EZ_PoolManager.Despawn(this.transform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using EZ_Pooling;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float MaxSpeed;
+    public float Acceleration;
+    public float RotationSpeed;
+    public float DelayBetweenShots;
+    public Transform Bullet;
+    public float ShotSpeed;
+
+    private float _timeSinceLastShot;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // movement 
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+	    if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0,0,RotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -RotationSpeed);
+        }
+	    
+	    if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // apply thrust 
+
+            // limit speed
+            rigidbody.AddForce(transform.up * Acceleration);
+
+            if (rigidbody.velocity.magnitude > MaxSpeed)
+            {
+                rigidbody.velocity = rigidbody.velocity.normalized * MaxSpeed;
+            }
+
+            // show engine boost particle effect
+            transform.Find("RightThrustPE").GetComponent<ParticleSystem>().Play();
+            transform.Find("LeftThrustPE").GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            // turn off engine particle effect
+            transform.Find("RightThrustPE").GetComponent<ParticleSystem>().Stop();
+            transform.Find("LeftThrustPE").GetComponent<ParticleSystem>().Stop();
+        }
+
+        //shooting
+	    _timeSinceLastShot += Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+        {
+            if (_timeSinceLastShot > DelayBetweenShots)
+            {
+                //shoot!
+                _timeSinceLastShot = 0f;
+
+                Transform bullet = EZ_PoolManager.Spawn(Bullet, transform.position, transform.rotation);
+
+                Rigidbody2D bulletRigidBody = bullet.GetComponent<Rigidbody2D>();
+                bulletRigidBody.velocity = transform.up * ShotSpeed;
+
+            }
+        }
+	}
+}
